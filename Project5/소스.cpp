8a3765273fec7f2888736cc6a530f6aace31c117
성민 표 <pmm7812@naver.com>
2019-11-22 15:09:19 +0900

    초기 커밋.

---
+++ Project5/소스.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void) {
+
+	char a = '*';
+	char* b = &a;
+
+	printf("변수 a의 주소 = %u, 코드값 = %d, 문자 = %c\n", &a, a, a);
+	printf("변수 a의 주소 = %u, 코드값 = %d, 문자 = %c\n", b, *b, *b);
+
+	return 0;
+}
